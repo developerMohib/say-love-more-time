24ea670a5488d20da794fd515820454457a5af13
Mohib <mohibullahmohim2020@gmail.com>
2024-03-25 23:59:11 +0600

    first commit

--- app.js
+++ app.js
@@ -9,20 +9,26 @@ function love(){
     playAudioButton.classList.add('design-b');
     stopAudioButton.classList.add('design-b');
     const text = document.getElementById('innterLove');
-    for (let i = 1; i <= 100; i++) {
-        console.log( `i love you : ${i}`);
+
+    for (let i = 1; i <= 1001; i++) {
+        console.log( `Happy Birth Day : ${i}`);
         const li = document.createElement('li');
-        li.innerText = `I Love You : ${i}`;
+        li.innerText = `Happy Birth Day : ${i}`;
         text.appendChild(li);
     }
     return text ;
 };
 
-const playAudio = () => new Audio("./audio/a-love.mp3").play();
+const playAudio = () => new Audio("").play();
 // const playAudio = () => new Audio("./audio/i-love-you.mp3").play();
 
 const stopAudio = () => {
-    let player = document.getElementById('stopAudio');
-    (player.paused == true) ? toggle(0) : toggle(1);
+    // let player = document.getElementById('stopAudio');
+    // (player.paused == true) ? toggle(0) : toggle(1);
+
+    love();
 
 };
+
+// let player = document.getElementById("radio");
+// player.onpause()
